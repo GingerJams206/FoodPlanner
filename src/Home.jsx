104f27b9a025ee38704caf7af5bbf8c561de8ca6
Sawyer Lightsey <sawyerlightsey@gmail.com>
2021-07-09 15:52:53 -0400

    moved firebase to context and rendering context on main page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,53 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import firebase from "./firebase/index"
+import React, { useState } from 'react'
 import RecipeCard from './components/RecipeCard'
 import RecipeForm from './RecipeForm'
-import { v4 as uuidv4 } from 'uuid'
+import * as RecipeContext from './RecipeContext'
 
 export default function Home() {
-  const [recipes, setRecipes] = useState([])
   const [loading, setLoading] = useState(false)
-  const db = firebase.db
-  const fbRecipes = db.collection('recipes')
-  // const initState = { title: '', description: '', ingredients: [{name: '', description: '', id: Math.random()}] };
   const initState = { title: '', description: '', ingredients: [] }
   const [recipe, setRecipe] = useState(initState)
 
-  useEffect(() => {
-    setRecipes([])
-    getRecipes()
-  }, [])
+  const recipes = RecipeContext.useRecipeContext()
+  const editRecipe = RecipeContext.useRecipeUpdateContext()
+  const addRecipe = RecipeContext.useRecipeAddContext()
+  const deleteRecipe = RecipeContext.useRecipeDeleteContext()
 
-
-  const getRecipes = () => {
-    fbRecipes.get()
-      .then(querySnapshot => {
-        querySnapshot.forEach(doc => {
-          setRecipes(prev => ([...prev, doc.data()]))
-        })
-      })
-      .catch(err => {
-        console.log(err.message)
-      })
+  const handleAddRecipe = (recipe) => {
+    addRecipe(recipe)
+    setRecipe(initState)
   }
-  //console.log(firebase)
 
-  const addRecipe = async (e) => {
-    e.preventDefault();
-    console.log(recipe)
-    if (recipe.ingredients.length === 0) return
-    
-    recipe.id = uuidv4();
-    console.log(recipe)
-    await fbRecipes.doc(recipe.id)
-      .set(recipe)
-      .then(() => {
-        setRecipes([])
-        setRecipe(initState)
-        getRecipes()})
-      .catch(error => {
-        console.log(error.message)
-      })
+  const handleEditRecipe = (recipe) => {
+    editRecipe(recipe)
+    setRecipe(initState)
+  }
+
+  const handleDeleteRecipe = (recipe) => {
+    deleteRecipe(recipe)
+    setRecipe(initState)
   }
 
   const clearRecipe = () => {
@@ -59,30 +37,6 @@ export default function Home() {
     setRecipe(prev => ({ ...prev, [name]: value }))
   }
 
-  const deleteRecipe = () => {
-    fbRecipes.doc(recipe.id).delete()
-      .then(() => {
-        setRecipes([])
-        setRecipe(initState)
-        getRecipes()
-      })
-      .catch((err) => {
-        console.error(err);
-      })
-  }
-
-  const editRecipe = () => {
-    fbRecipes.doc(recipe.id)
-      .update(recipe)
-      .then(() => {
-        setRecipes([])
-        getRecipes()
-      })
-      .catch((err) => {
-        console.error(err)
-      });
-  }
-
   return (
     <div>
       <h1>Home Page</h1>
@@ -91,11 +45,11 @@ export default function Home() {
           null :
           recipes.map((recipe, i) => <h1 key={i} onClick={() => setRecipe(recipe)}><RecipeCard recipe={recipe} /> </h1>)
       }
-      <RecipeForm handleChange={handleChange} recipe={recipe} setRecipe = {setRecipe} />
+      <RecipeForm handleChange={handleChange} recipe={recipe} setRecipe={setRecipe} />
       <br />
-      <button  onClick={recipe?.id ? editRecipe : addRecipe}>{recipe?.id ? 'Update Recipe' : 'Add Recipe'}</button>
+      <button onClick={recipe?.id ? () => handleEditRecipe(recipe) : () => handleAddRecipe(recipe)}>{recipe?.id ? 'Update Recipe' : 'Add Recipe'}</button>
       <button onClick={clearRecipe}>Clear Recipe</button>
-      <button onClick={deleteRecipe} disabled = {recipe?.id ? false : true}>Delete Recipe</button>
+      <button onClick={() => handleDeleteRecipe(recipe)} disabled={recipe?.id ? false : true}>Delete Recipe</button>
     </div>
   )
 }
